d168a4ac161ed614b91b3e610de440602b3baed2
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-05-10 17:51:19 -0500

    Add challenge 07

---
+++ 2021/challenge-07/solution.js
@@ -0,0 +1,44 @@
+function contains(store, product) {
+  for (const key in store) {
+    if (store[key] === product) return true
+    else {
+      if (typeof store[key] === 'object') {
+        if (contains(store[key], product)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+const almacen = {
+  'estanteria1': {
+    'cajon1': {
+      'producto1': 'coca-cola',
+      'producto2': 'fanta',
+      'producto3': 'sprite'
+    }
+  },
+  'estanteria2': {
+    'cajon1': 'vacio',
+    'cajon2': {
+      'producto1': 'pantalones',
+      'producto2': 'camiseta' // <- ¡Está aquí!
+    }
+  }
+}
+
+
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
+    }
+  }
+}
+
+contains(almacen, 'camiseta') // true
+contains(otroAlmacen, 'gameboy') // false
