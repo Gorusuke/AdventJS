8b234f37c9c371ff8a3e5afeb243b9e9de3d06ca
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-12-09 19:50:59 -0500

    Add challenge 09

--- 2024/challenge-09/solution.js
+++ 2024/challenge-09/solution.js
@@ -14,7 +14,6 @@ function moveTrain(board, mov) {
     }
   }
 
-  console.log(trainPositions)
   const moveMap = {
     'L': [0, -1],
     'R': [0, 1],
@@ -59,14 +58,14 @@ console.log(moveTrain(board, 'U'))
 // ➞ 'eat'
 // Porque el tren se mueve hacia arriba y encuentra una fruta mágica
 
-// console.log(moveTrain(board, 'D'))
+console.log(moveTrain(board, 'D'))
 // ➞ 'crash'
 // El tren se mueve hacia abajo y la cabeza se choca consigo mismo
 
-// console.log(moveTrain(board, 'L'))
+console.log(moveTrain(board, 'L'))
 // ➞ 'crash'
 // El tren se mueve a la izquierda y se choca contra la pared
 
-// console.log(moveTrain(board, 'R'))
+console.log(moveTrain(board, 'R'))
 // ➞ 'none'
 // El tren se mueve hacia derecha y hay un espacio vacío en la derecha
