f5b750566b48dd3c2ec30c5ef4082c5b6eec4ddf
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-12-21 08:27:41 -0500

    Add challenge 21

---
+++ 2024/challenge-21/solution.js
@@ -0,0 +1,39 @@
+function treeHeight(tree) {
+  if (!tree) return 0
+  return 1 + Math.max(treeHeight(tree.left), treeHeight(tree.right))
+}
+
+const tree = {
+  value: '🎁',
+  left: {
+    value: '🎄',
+    left: {
+      value: '⭐',
+      left: null,
+      right: null
+    },
+    right: {
+      value: '🎅',
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: '❄️',
+    left: null,
+    right: {
+      value: '🦌',
+      left: null,
+      right: null
+    }
+  }
+}
+
+console.log(treeHeight(tree)) // Devuelve: 3
+// Representación gráfica del árbol:
+//        🎁
+//       /   \
+//     🎄     ❄️
+//    /  \      \
+//  ⭐   🎅      🦌
+// tiene 3 niveles de profundidad
